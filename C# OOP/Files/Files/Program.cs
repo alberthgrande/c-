6a952f6bd595009b3aceb99583fe6967dab4ac93
alberthgrande <alberthgrande98@gmail.com>
2026-03-26 20:24:57 +0800

    Files

---
+++ C# OOP/Files/Files/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.IO;
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string writeText = "Hello, World!";
+            File.WriteAllText("filename.txt", writeText);
+
+            string readText = File.ReadAllText("filename.txt");
+            Console.WriteLine(readText);
+        }
+    }
+}
