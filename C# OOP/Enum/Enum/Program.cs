84dfbd354a2aff7f00bae4231cc890b562150b7e
alberthgrande <alberthgrande98@gmail.com>
2026-03-26 19:45:04 +0800

    Enum

---
+++ C# OOP/Enum/Enum/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Enum
+{
+    enum Level
+    {
+        Low,
+        Medium,
+        High
+    }
+
+    enum Months
+    {
+        January,
+        February,
+        March,
+        April,
+        May,
+        June,
+        July,
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Level myLevel = Level.Medium;
+            Console.WriteLine(myLevel);
+
+            int myMonths = (int)Months.July;
+            Console.WriteLine(myMonths);
+
+            switch (myLevel)
+            {
+                case Level.Low:
+                    Console.WriteLine("Low Level");
+                    break;
+                case Level.Medium:
+                    Console.WriteLine("Medium Level");
+                    break;
+                case Level.High:
+                    Console.WriteLine("High");
+                    break;
+                default:
+                    Console.WriteLine("Invalid Level");
+                    break;
+            }
+        }
+    }
+}
