444edeaedbad78f01281a0cadbad288ad379eb4c
alberthgrande <alberthgrande98@gmail.com>
2026-03-27 17:56:32 +0800

    C# OOP/Exceptions

---
+++ C# OOP/Exceptions/Exceptions/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+
+static void checkAge(int age)
+{
+
+    try
+    {
+        if (age < 18)
+        {
+            throw new ArithmeticException("Access denied - You must be at least 18 years old.");
+        }
+        else
+        {
+            Console.WriteLine("Access granted - You are old enough!");
+        }
+    }
+    catch (System.Exception)
+    {
+        throw;
+    }
+    finally
+    {
+        Console.WriteLine("The 'try catch' is finished");
+    }
+}
+
+checkAge(50);
